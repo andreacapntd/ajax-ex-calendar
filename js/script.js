fddad95e99b6c2265546b687f954e24eab0d4dd6
andreacapntd <andreacapntd@gmail.com>
2020-07-23 14:46:23 +0200

    primo commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Creiamo un calendario dinamico con le festività.
+// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull'API).
+// Milestone 1
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function init() {
+
+  console.log('hello');
+  
+}
+
+
+
+
+
+$(document).ready(init);
