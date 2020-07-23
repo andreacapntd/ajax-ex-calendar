55460691ef00dc85a690674fe1fd945297e4d654
Andrea Caprara <andreacaprara85@gmail.com>
2020-07-23 19:18:20 +0200

    stampa dei giorni a schermo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,59 @@
 // Milestone 1
 // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
 
+//API: https://flynn.boolean.careers/exercises/api/holidays
 
+// PARAMS:
+// year: 2018
+// month: 0 ~ 11
+
+function getCalendarAjaxMoment() {
+
+  var month = 0;
+
+  $.ajax({
+
+    url: 'https://flynn.boolean.careers/exercises/api/holidays',
+    method: 'GET',
+    data: {
+
+      'month': month,
+      'year': 2018
+
+    },
+    success: function(data) {
+
+      var success = data['success'];
+      var date = data['response'];
+      console.log(date);
+
+      var target = $('#days')
+
+
+
+      for (var i = 1; i <= 31 ; i++) {
+
+        var newOpt = '<li value="' + i + '">' + i + '</li>'
+        target.append(newOpt);
+      }
+
+      for (var i = 0; i < date.length; i++) {
+
+        var holidays = date[i];
+        var holidayName = holidays['name'];
+        var holidayDate = holidays['date'];
+        var mom = moment(holidayDate, 'YYYY/MM/DD');
+        console.log(mom.format('YYYY/MM/DD'));
+
+      }
+
+    },
+    error: function() {
+
+      console.log('error');
+    }
+  })
+}
 
 
 
@@ -22,8 +74,8 @@
 
 function init() {
 
-  console.log('hello');
-  
+  getCalendarAjaxMoment();
+
 }
 
 
